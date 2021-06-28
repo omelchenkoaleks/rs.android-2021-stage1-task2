4cc32a7d173a9955e6ee8b82eef33925b616945d
omelchenkoaleks <omelchenkoaleks@gmail.com>
2021-06-28 06:16:24 +0300

    refactoring Task2BlocksTest

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,23 +1,62 @@
 package subtask3
 
+import java.time.LocalDate
 import kotlin.reflect.KClass
 
 class Blocks {
 
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
 
-        when (blockB.qualifiedName) {
-            is String -> {
+        when (blockB.simpleName) {
+            "String" -> {
+                println("Sting")
                 val stringBuilder: StringBuilder = java.lang.StringBuilder()
                 blockA.forEach {
                     if (it is String) {
-                        println(it)
                         stringBuilder.append(it)
                     }
                 }
                 return stringBuilder.toString()
             }
+            "Int" -> {
+                println("Int")
+                var sum = 0
+                blockA.forEach {
+                    if (it is Int) {
+                        sum += it
+                    }
+                }
+                return sum
+
+            }
+            "LocalDate" -> {
+                val arrayDate = getListLocalDate(blockA)
+                var nearestDate = arrayDate[0]
+                for (i in arrayDate.indices - 2) {
+                    if (arrayDate[i] > arrayDate[i + 1]) {
+                        nearestDate = arrayDate[i]
+                    }
+                }
+                return formatDate(nearestDate)
+            }
         }
         return ""
+    }
+
+    private fun formatDate(date: LocalDate): String {
+        val year = date.year
+        val month = date.monthValue
+        val dayOfMonth = date.dayOfMonth
+        return "$dayOfMonth.$month.$year"
+    }
+
+    private fun getListLocalDate(block: Array<*>): Array<LocalDate> {
+        val arrayDate = mutableListOf<LocalDate>()
+        block.forEach {
+            if (it is LocalDate) {
+                arrayDate.add(it)
+            }
+        }
+        return arrayDate.toTypedArray()
     }
 }
